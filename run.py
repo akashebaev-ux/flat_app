aef362715eed87dcbe0bb1adcd7c03c4cb534398
akashebaev-ux <akashebaev@gmail.com>
2026-03-04 17:51:54 +0100

    Fix Chrome path for Heroku

--- run.py
+++ run.py
@@ -23,7 +23,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # Selenium is a powerful tool for web scraping and browser automation.
@@ -66,17 +65,16 @@ def start_driver():
     print("Starting Chrome browser...")
 
     options = webdriver.ChromeOptions()
-    options.binary_location = "/app/.apt/usr/bin/google-chrome"
+
+    options.binary_location = "/app/.chrome-for-testing/chrome-linux64/chrome"
 
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(
-        service=Service("/app/.chromedriver/bin/chromedriver"),
-        options=options
-    )
+    driver = webdriver.Chrome(options=options)
 
     return driver
 
